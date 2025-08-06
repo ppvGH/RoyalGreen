254092c117cec3d4758f5032edf5e2f67e61bf50
ppv <pierpaolo.vesce@studio.unibo.it>
2025-08-06 23:07:06 +0200

    Made Shader::compile private.\n Now Shader::linkProgram returns *this so storing shaders is possible without copying them.\n Add a destructor.  Add uniform setter for matrices.

--- src/core/shader.cpp
+++ src/core/shader.cpp
@@ -1,33 +1,13 @@
 #include "shader.h"
 
 #include <glad/glad.h>
+
+#include <glm/gtc/type_ptr.hpp>		// for glm::value_ptr()
 #include <iostream>
 
 
 
-
-
-unsigned int Shader::compile(unsigned int type, const std::string& source)
-{
-	// shader identifier
-	unsigned int id;
-
-	// std::string to C-string conversion
-	const char* src = source.c_str();
-
-	// create and compile shader
-	id = glCreateShader(type);
-	glShaderSource(id, 1, &src, NULL);
-	glCompileShader(id);
-	
-	// compile error check
-	compileErrorCheck(id, type);
-
-	return id;
-
-}
-
-void Shader::linkProgram(const std::string& srcVert, const std::string& srcFrag)
+Shader& Shader::linkProgram(const std::string& srcVert, const std::string& srcFrag)
 {
 	// compile vertex and fragment shaders
 	unsigned int vs = compile(GL_VERTEX_SHADER, srcVert);
@@ -42,14 +22,57 @@ void Shader::linkProgram(const std::string& srcVert, const std::string& srcFrag)
 	// link error check
 	linkErrorCheck();
 	
+	// delete shaders after linking
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return *this;
 }
 
-Shader& Shader::use() 
+Shader& Shader::use()
 {
 	glUseProgram(ID);
-
 	// returns *this for chaining [e.g. shader.use().setInt(...)]
 	return *this;
+}
+
+unsigned int Shader::getID() const
+{
+	return ID;
+}
+
+void Shader::setUniformMatrix4fv(const std::string& name, int count, bool transpose, const glm::mat4& matrix) const
+{
+	glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), count, transpose, glm::value_ptr(matrix));
+}
+
+Shader::~Shader()
+{
+	// if exists it deletes it
+	if (ID != 0) 
+	{
+		glDeleteProgram(ID);
+	}
+}
+
+unsigned int Shader::compile(unsigned int type, const std::string& source)
+{
+	// shader identifier
+	unsigned int id;
+
+	// std::string to C-string conversion
+	const char* src = source.c_str();
+
+	// create and compile shader
+	id = glCreateShader(type);
+	glShaderSource(id, 1, &src, NULL);
+	glCompileShader(id);
+
+	// compile error check
+	compileErrorCheck(id, type);
+
+	return id;
+
 }
 
 void Shader::compileErrorCheck(unsigned int id, unsigned int type) const
@@ -67,8 +90,8 @@ void Shader::compileErrorCheck(unsigned int id, unsigned int type) const
 	if (!success)
 	{
 		glGetShaderInfoLog(id, 1024, NULL, infoLog);
-		std::cerr << "ERROR::SHADER: Compile-time error: " << errMsg << "\n" << infoLog 
-				  << "\n -- --------------------------------- --" << std::endl;
+		std::cerr << "ERROR::SHADER: Compile-time error: " << errMsg << "\n" << infoLog
+			<< "\n -- --------------------------------- --" << std::endl;
 	}
 }
 
@@ -82,7 +105,6 @@ void Shader::linkErrorCheck() const
 	{
 		glGetProgramInfoLog(ID, 1024, NULL, infoLog);
 		std::cerr << "ERROR::SHADER: Link-time error: SHADER PROGRAM FAILURE\n" << infoLog
-				  << "\n -- --------------------------------- --" << std::endl;
+			<< "\n -- --------------------------------- --" << std::endl;
 	}
 }
-
--- src/core/shader.h
+++ src/core/shader.h
@@ -1,5 +1,7 @@
 #pragma once
+#include <glm/glm.hpp>
 #include <string>
+
 
 class Shader
 {
@@ -7,31 +9,39 @@ public:
 	/* Default constructor: initalizes ID to 0 */
 	Shader() : ID(0) {};
 
-	/* Creates and compiles a shader from a string
-	 > returns the shader id;
-	 - type can only be "GL_VERTEX_SHADER" or "GL_FRAGMENT_SHADER"; */
-	unsigned int compile(unsigned int type, const std::string& source);
-
-	/* Creates, compiles and links shader program from vertex and fragment in a string format */
-	void linkProgram(const std::string& srcVert, const std::string& srcFrag);
+	/* Creates, compiles and links shader program from vertex and fragment in a string format 
+	 - arguments are given one by one to the compile private member function 
+	 > returns *this to load shader programs in resource manager avoiding copies */
+	Shader& linkProgram(const std::string& srcVert, const std::string& srcFrag);
 
 	/* Activates shader
 	 > returns *this */
 	Shader& use();
 
-	/* Uniforms */
-	// TODO
+	/* Getter for the shader program's ID */
+	unsigned int getID() const;
+
+	/* TODO: Uniforms */
+	void Shader::setUniformMatrix4fv(const std::string& name, int count, bool transpose, const glm::mat4& matrix) const;
+	
+	/* Destructor */
+	~Shader();
 
 
 private:
 	// shader program identifier	
 	unsigned int ID;
 
+	/* Creates and compiles a shader from a string
+	 > returns the shader id;
+	 - type can only be "GL_VERTEX_SHADER" or "GL_FRAGMENT_SHADER"; */
+	unsigned int compile(unsigned int type, const std::string& source);
+
 	/* Shader compile error check function.
 	 - id is the shader identifier;
-	 - type can only be "GL_VERTEX_SHADER" or "GL_FRAGMENT_SHADER"; "*/
+	 - type can only be "GL_VERTEX_SHADER" or "GL_FRAGMENT_SHADER"; */
 	void compileErrorCheck(unsigned int id, unsigned int type) const;
 
 	/* Shader program link error check function.*/
-	void linkErrorCheck() const ;
+	void linkErrorCheck() const;
 };
