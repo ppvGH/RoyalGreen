7e88bcb3aca1011d1e2d8b6477e30f2780ce4a0c
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-01 00:42:12 +0200

    feat(game2D): Implementation of game class

--- src/app/game2D/game.cpp
+++ src/app/game2D/game.cpp
@@ -1,1 +1,86 @@
 #include "game.h"
+
+#include "game_data.h"
+
+#include "../../core/resource_manager.h"
+#include "../../core/commonGL.h"
+#include <glm/gtc/matrix_transform.hpp> //glm::translate
+#include <iostream>
+
+Game::Game(int width, int height) :
+    m_width(width),
+    m_height(height),
+    m_player(nullptr),
+    m_background(nullptr),
+	m_FBO(width, height),
+	m_spriteRenderer(width, height),
+    m_shader(ResourceManager::getShader("tex2D")),
+    m_cameraPosition(0.0f),
+    m_cameraMargin(0.0f)
+{
+    initGame();
+}
+
+void Game::initGame()
+{   
+    /* Initial speed is 0.0f. */
+    m_player = new Player(gameData::playerTexName, gameData::playerPosition, gameData::playerSize, 0.0f); 
+
+    m_background = new Background(gameData::backgroundTexName, gameData::backgroundPosition, gameData::backgroundSize);
+
+    /* Cam centered on the player; dead zone is set to be twice as large as the character sprite. */
+    m_cameraPosition = m_player->getPosition().x;
+    m_cameraMargin = m_player->getSize().x * 2.0;
+}
+
+void Game::input2DHandler(const ActionMap& actionMap2D, float dt)
+{
+    m_player->inputHandler(actionMap2D, dt);
+}
+
+
+void Game::update(float dt)
+{
+    updateCamera();
+
+    m_player->update(dt);
+}
+
+
+
+void Game::render() const
+{
+    /* Framebuffer and viewport setup. */
+    m_FBO.bind();
+    glViewport(0, 0, m_FBO.getWidth(), m_FBO.getHeight());
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+    /* Camera setup. */
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-m_cameraPosition, 0.0f, 0.0f));
+    m_shader.use();
+    m_shader.setMatrix4fv("view", 1, GL_FALSE, view);
+
+    /* Rendering game elements. */
+    m_background->render(m_spriteRenderer, m_shader);
+    m_background->render(m_spriteRenderer, m_shader, 2.0f * gameData::backgroundPosition);
+    m_player->render(m_spriteRenderer, m_shader);
+
+
+    /* Detaches the FBO. */
+    m_FBO.unbind();
+}
+
+
+void Game::updateCamera()
+{
+    /* Right border: (P > C + (W - P)) ---> C = P - (W - P). */
+    /* Left border:  (P < C + M )    ------> C = P - M. */
+    if (m_player->getPosition().x > m_cameraPosition + m_width - m_cameraMargin)
+        m_cameraPosition = m_player->getPosition().x - (m_width - m_cameraMargin);
+    else if (m_player->getPosition().x < m_cameraPosition + m_cameraMargin)
+        m_cameraPosition = m_player->getPosition().x - m_cameraMargin;
+
+    /* Bounds cam position to be non-negative. */
+    if (m_cameraPosition < 0.0f) m_cameraPosition = 0.0f;
+}
--- src/app/game2D/game.h
+++ src/app/game2D/game.h
@@ -1,5 +1,59 @@
 #pragma once
 
+#include "framebuffer.h"
 #include "sprite_renderer.h"
-#include "../../graphics/framebuffer.h"
+#include "player.h"
+#include "background.h"
 
+#include "../../core/action_map.h"
+
+class Game 
+{
+public:
+
+
+	Game() = delete;
+
+	Game(int width, int height);
+
+	void initGame();
+
+	void input2DHandler(const ActionMap& actionMap2D, float dt);
+
+	void update(float dt);
+
+	void render() const;
+
+
+
+	
+
+	/* Getters. */
+	//const Framebuffer& getFBO() const { return m_FBO; }		//TODO: NON UTILIZZATO; CANCELLARE?
+	const SpriteRenderer& getSpriteRenderer() const { return m_spriteRenderer; }
+	const Texture& getFBOTex() const { return m_FBO.getTex(); }
+
+private:
+
+	/* Internal variables.*/
+	float m_width, m_height;
+
+
+	/* Game items. */
+	Player* m_player;
+	Background* m_background;
+
+	/* Camera is just an X coordinate for now. */
+	float m_cameraPosition;
+	/* Limit of camera view. */
+	float m_cameraMargin;
+	/* Updates camera position. */
+	void updateCamera();
+
+
+
+
+	Framebuffer m_FBO;
+	SpriteRenderer m_spriteRenderer;
+	Shader& m_shader;
+};
