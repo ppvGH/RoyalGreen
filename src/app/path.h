62ab44405fec1d38b16e4b0ac1a7206cb21dad00
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-01 00:50:55 +0200

    feat(app): Add a unique path file to store all filepaths needed

---
+++ src/app/path.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <string>
+
+/* Paths to shader source files.
+ * <X>_DIR are in the CMakeLists.txt, note that they end with a / character.
+ * SHADERS_DIR = "${CMAKE_SOURCE_DIR}/res/shaders/"
+ * MESHES_DIR = "${CMAKE_SOURCE_DIR}/res/meshes/" */
+namespace Path
+{
+	inline const std::string pathVert = std::string(SHADERS_DIR) + "basic.vert";
+	inline const std::string pathFrag = std::string(SHADERS_DIR) + "basic.frag";
+	inline const std::string path2DVert = std::string(SHADERS_DIR) + "basic2D.vert";
+	inline const std::string path2DFrag = std::string(SHADERS_DIR) + "basic2D.frag";
+	inline const std::string pathTex2DVert = std::string(SHADERS_DIR) + "tex2D.vert";
+	inline const std::string pathTex2DFrag = std::string(SHADERS_DIR) + "tex2D.frag";
+
+	inline const std::string pathModel = std::string(MESHES_DIR) + "tempArcade.obj";
+	inline const std::string pathRoom = std::string(MESHES_DIR) + "room.obj";
+
+	inline const std::string pathRospi = std::string(TEXTURE_DIR) + "rospi.png";
+	inline const std::string pathPlayerSprite = std::string(TEXTURE_DIR) + "player_spritesheet.png";
+	inline const std::string pathBackground = std::string(TEXTURE_DIR) + "background.png";
+}
