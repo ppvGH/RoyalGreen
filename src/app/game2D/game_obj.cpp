388763c3a4fa2dc2aaee47c218dbe4accc3cc413
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-01 00:45:54 +0200

    chore(game2D): Removed some comments

--- src/app/game2D/game_obj.cpp
+++ src/app/game2D/game_obj.cpp
@@ -12,8 +12,4 @@ GameObj::GameObj(glm::vec2 position, glm::vec2 size, glm::vec2 velocity) :
 void GameObj::updatePosition(float dt)
 {
 	m_position += m_velocity * dt;
-	/* For certain elements like the player it doesnt seem to be the real 0 (the left border of the screen),
-	 * but that is just because sprite transparency occupies a consistent portion of the quad. */
-	//if (m_position.x < 0.0f) m_position.x = 0.0f;
-	//if (m_position.y < gameData::groundLevel) m_position.y = gameData::groundLevel;
 }
