317d8595cd471650e5c358f10965dafb81a4320c
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-02 12:07:46 +0200

    feat(sceneData): Add some values

--- src/app/scene_data.h
+++ src/app/scene_data.h
@@ -1,6 +1,26 @@
 #pragma once
 
+#include <glm/glm.hpp>
+
 namespace sceneData
 {
+
+	/* Camera data. */
+	inline const glm::vec3 cameraLightPosition = glm::vec3(20.0f);
+
+
+
+	/* Camera animation data. */
+	inline const float inAnimationFirstPartDuration = 2.0f;
+	inline const float inAnimationSecondPartDuration = 1.0f;
+
+
+
+
+
+
+
+
+	/* Old data. */
 	inline const float arcadeAnimationSpeed = 0.005f;
 }
