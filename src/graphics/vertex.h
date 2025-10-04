915058cd5fb01fdcb5e69f31e1b858d6ebdfd701
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-04 15:07:31 +0200

    feat(vertex): Add getters

--- src/graphics/vertex.h
+++ src/graphics/vertex.h
@@ -5,20 +5,28 @@
 /* Basic struct representing a vertex passed to the GPU. */
 struct Vertex
 {
-	glm::vec3 position;
-	glm::vec3 normal;
-	glm::vec2 texCoords;
+	glm::vec3 m_position;
+	glm::vec3 m_normal;
+	glm::vec2 m_texCoords;
 
-	Vertex() :
-		position(glm::vec3(0.0f)),
-		normal(glm::vec3(0.0f)),
-		texCoords(glm::vec2(0.0f))
+	const glm::vec3& getPosition() const { return m_position; }
+	const glm::vec3& getNormal() const { return m_normal; }
+	const glm::vec2& getTexCoords() const { return m_texCoords; }
+
+	Vertex() = delete;
+
+	Vertex(glm::vec3 position, glm::vec3 normal, glm::vec2 texCoords) :
+		m_position(position),
+		m_normal(normal),
+		m_texCoords(texCoords)
 	{
 	}
+
+
 };
 
 /* Struct over class choice is for simplicity and because of
  * the usage of the "offsetof" macro in the Mesh setup method. 
  * Classes can give undefined behaviours with the offsetof macro,
  * even if in this particular case it could still work correctly,
- * as it is a POD (Plain Old Data) class. */
+ * as it would be a POD (Plain Old Data) class. */
