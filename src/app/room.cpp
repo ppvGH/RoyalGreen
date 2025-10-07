284aefcd591cc93adb9ff1433edd6269cbbe7772
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-07 12:46:37 +0200

    feat(room): New class to manage room model object (needed for render floor differently

---
+++ src/app/room.cpp
@@ -0,0 +1,24 @@
+#include "room.h"
+#include "scene_data.h"
+
+Room::Room(const std::string& pathModel, Texture* floorTex):
+	m_model(pathModel),
+	m_floorTex(floorTex)
+{
+	if (floorTex != nullptr) setFloor(*floorTex);
+}
+
+
+void Room::setFloor(const Texture& texture)
+{
+	/* Tiling setup: mesh UV resizing. */
+	Mesh& meshFloor = m_model.getMesh(sceneData::meshFloorName);
+	meshFloor.resizeUV(sceneData::floorTileFactor);
+
+	/* Override material with an external texture. */
+	Material& matFloor = m_model.getMaterial(sceneData::matFloorName);
+	matFloor.overrideTex(texture);
+
+	/* Texture is fixed and not switchable. */
+	matFloor.setUseTex(true);
+}
---
+++ src/app/room.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../graphics/model.h"
+
+class Room
+{
+public:
+
+	/* No default constructor allowed. */
+	Room() = delete;
+
+	/* Creates a Room object from its filepath and a texture for its floor if given. */
+	Room(const std::string& pathModel, Texture* floorTex = nullptr);
+
+	/* Overrides floor material with a texture. */
+	void setFloor(const Texture& texture);
+
+	/* Draws the model. */
+	void draw(Shader& shader) const { m_model.draw(shader); }
+
+	/* Model getter (non-const). */
+	Model& getModel() { return m_model; }
+	/* Model getter (read-only) .*/
+	const Model& getModel() const { return m_model; }
+
+private:
+
+	Model m_model;
+
+	Texture* m_floorTex;
+
+};
