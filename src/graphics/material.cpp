5327cd2c3f68beaf09585634858df6c845e87ac9
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-07 12:49:14 +0200

    fix(material): Renamed a uniform

--- src/graphics/material.cpp
+++ src/graphics/material.cpp
@@ -1,7 +1,6 @@
 #include "material.h"
 
 #include <glad/glad.h>
-
 Material::Material() :
 	m_matName(""),
 	m_ambient(glm::vec3(0.0f)),
@@ -18,23 +17,21 @@ Material::Material() :
 void Material::apply(Shader& shader) const
 {
 	/* Names of the uniforms should match ones in the shader. */
-	shader.setVector3f("matAmbient", m_ambient);
+	shader.setVector3f("matAmbient", m_ambient); // TODO: non ha molto senso usare ambient su ogni singolo materiale dato che 
 	shader.setVector3f("matDiffuse", m_diffuse);
 	shader.setVector3f("matSpecular", m_specular);
 	shader.setVector3f("matEmission", m_emission);
 	shader.setFloat("matShininess", m_shininess);
 
-
 	if (m_useTex && m_texture!=nullptr)
 	{
 		glActiveTexture(GL_TEXTURE0);
 		m_texture->bind();
-		shader.setInt("useScreenTex", 1);
-		//shader.setInt("screenTex", 0);
+		shader.setInt("useTex", 1);
 	}
 	else 
 	{
-		shader.setInt("useScreenTex", 0);
+		shader.setInt("useTex", 0);
 	}
 }
 
