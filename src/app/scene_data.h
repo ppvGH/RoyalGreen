f0d6b9511eb60415a792c4faaa8ff59e543f84f1
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-07 12:47:50 +0200

    feat(sceneData): Add some previously hardcoded values

--- src/app/scene_data.h
+++ src/app/scene_data.h
@@ -1,13 +1,19 @@
 #pragma once
 
+#include <string>
 #include <glm/glm.hpp>
 
 namespace sceneData
 {
-	/* Model data. */
-	const glm::vec3 arcadeModelPositionShift = glm::vec3(-4.0f, 0.02f, 0.0f); // 0.02 is to lift the model a tiny bit.
+	/* Arcade model data. */
+	inline const std::string matScreenName = "display";
+	inline const std::string matPowerButtonName = "power_button_power_button";
+	inline const glm::vec3 arcadeModelPositionShift = glm::vec3(-4.0f, 0.02f, 0.0f); // 0.02 is to lift the model a tiny bit.
 
-
+	/* Room model data.*/
+	inline const std::string matFloorName = "floor";
+	inline const std::string meshFloorName = "floor";
+	inline const float floorTileFactor = 16.0f; // 10/16 = 0.667 (size of the tile)
 
 	/* Camera data. */
 	inline const glm::vec3 cameraLightPosition = glm::vec3(20.0f);
