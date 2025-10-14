396dafb315785efb9a4ae732a0fd32bfcaaf89c1
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-14 10:35:42 +0200

    feat(sceneData): Add meshes names, lamp stuff, changed camera light and starting position

--- src/app/scene_data.h
+++ src/app/scene_data.h
@@ -6,19 +6,23 @@
 namespace sceneData
 {
 	/* Arcade model data. */
+	inline const std::string meshScreenName = "display";
 	inline const std::string matScreenName = "display";
 	inline const std::string matPowerButtonName = "power_button_power_button";
-	inline const glm::vec3 arcadeModelPositionShift = glm::vec3(-4.0f, 0.02f, 0.0f); // 0.02 is to lift the model a tiny bit.
+	inline const glm::vec3 arcadeModelPositionShift = glm::vec3(-2.4f, 0.02f, 0.0f); // 0.02 is to lift the model a tiny bit.
 
 	/* Room model data.*/
-	inline const std::string matFloorName = "floor";
 	inline const std::string meshFloorName = "floor";
+	inline const std::string matFloorName = "floor";
 	inline const float floorTileFactor = 16.0f; // 10/16 = 0.667 (size of the tile)
 
+	/* Lamp model data. */
+	inline const glm::vec3 lampModelPositionShift = glm::vec3(0.0f, 2.5f, 0.0f);
+
 	/* Camera data. */
-	inline const glm::vec3 cameraLightPosition = glm::vec3(20.0f);
+	inline const glm::vec3 cameraLightPosition = glm::vec3(0.0f, 2.5f, 0.0f);
 	inline const float cameraAltitude = 1.7f;
-	inline const glm::vec3 cameraStartingPosition = glm::vec3(4.0f, cameraAltitude, 0.0f);
+	inline const glm::vec3 cameraStartingPosition = glm::vec3(2.0f, cameraAltitude, 0.0f);
 	inline const float cameraFovy = 45.0f;				// In degrees.
 	inline const float cameraAspect = 16.0f / 9.0f;		// default for now is same ratio as my display. TODO: check user display ratio
 	inline const float cameraNearPlane = 0.1f;
@@ -27,17 +31,12 @@ namespace sceneData
 	inline const float cameraInitMouseSpeed = 0.001f;
 	inline const float cameraInitKeyboardSpeed = 0.15f;
 
-
 	/* Camera animation data. */
 	inline const float inAnimationFirstPartDuration = 2.0f;
 	inline const float inAnimationSecondPartDuration = 1.0f;
 
-
 	/* Geometry data. */
 	inline const float intersectionEpsilon = 1e-6f;
-
-
-
 
 	/* Old data. */
 	inline const float arcadeAnimationSpeed = 0.005f;
