08acf41a4bad6d73ea6c8b61eab82ae62fac11b3
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-31 18:39:25 +0100

    feat(input): Modified input system: inputHandler manages only inputs and update func manages states evolution

--- src/app/game2D/player.cpp
+++ src/app/game2D/player.cpp
@@ -1,39 +1,56 @@
 #include "player.h"
 #include "sprite_renderer.h"
 #include "game_data.h"
+#include "projectile_manager.h"
 
 #include "../../core/utilities.h"
 #include <iostream>
 
-Player::Player(const std::string texName, glm::vec2 position, glm::vec2 size, float walkSpeed):
-	m_body(position, size, glm::vec2(0.0f)),
+Player::Player(const std::string& texName, glm::vec2 position, glm::vec2 size, glm::vec2 velocity):
+	m_body(position, size, velocity),
 	m_sprite(texName, gameData::playerSheetRows, gameData::playerSheetColumns),
 	m_state(State::Idle),
 	m_prevState(State::Walk),		// different from m_state to start sprite animations
 	m_facingRight(true),
-	m_canWalk(true)
+	m_onGround(true),
+	m_canMove(true),
+	m_shotFired(false)
 {
 }
 
-void Player::inputHandler(const ActionMap& actionMap2D, float dt)
+void Player::inputHandler(const ActionMap& actionMap2D, float dt, ProjectileManager& projectilesSys)
 {
+	if (!m_canMove) return;
+
 	if (isIdle(actionMap2D)) atIdle();
 	if (actionMap2D.ongoing(Action::P1MoveRight)) walk(true);
 	if (actionMap2D.ongoing(Action::P1MoveLeft)) walk(false);
-	if (actionMap2D.justStarted(Action::P1Jump) || m_state == State::Jump) jump(dt);
+
+	if ((actionMap2D.justStarted(Action::P1Jump))) setJump();
+
+	if (actionMap2D.justStarted(Action::P1Attack)) setAttack();
+
+	//printNewState(actionMap2D);
 }
 
 bool Player::isIdle(const ActionMap& actionMap2D)
 {
-	return !actionMap2D.ongoing(Action::P1MoveRight) &&
-		   !actionMap2D.ongoing(Action::P1MoveLeft) &&
-		   !(actionMap2D.ongoing(Action::P1Jump) || m_state == State::Jump);
+	/* P1 is Idle if its state is Idle or if there aren't any ongoing actions (walk, jump, attack). */
+
+	/* Actions condition. */
+	bool actions = !(actionMap2D.ongoing(Action::P1MoveRight) || actionMap2D.ongoing(Action::P1MoveLeft)); // walking
+	/* States condition. */
+	bool states = !(m_state == State::Jump || m_state == State::Attack);
+
+	return (actions && states) || m_state == State::Idle;
 }
 
 void Player::atIdle()
 {
-	m_body.setVelocity(glm::vec2(0.0f));
 	m_state = State::Idle;
+	m_body.setVelocity(glm::vec2(0.0f));
+	m_canMove = true;
+	m_onGround = true;		// TODO check mechanism if build platforms
 }
 
 void Player::walk(bool facingRight)
@@ -47,21 +64,27 @@ void Player::walk(bool facingRight)
 	 * but that is just because sprite transparency occupies a consistent portion of the quad. */
 	if (m_body.getHorizontalPosition() < 0.0f) m_body.setHorizontalPosition(0.0f);
 
-	/* Can move while jumping. */
+	/* Can move while jumping (horizontal velocity is set even when canWalk is false). */
 	if(m_state != State::Jump) m_state = State::Walk;
 }
 
-void Player::jump(float dt)
+void Player::setJump()
 {
 	/* First time entering the function: setting vertical speed to maxJumpSpeed and state to Jump. */
-	if (m_state != State::Jump && m_canWalk)
+	if (m_state != State::Jump && m_onGround)
 	{
 		m_body.setVerticalVelocity(gameData::playerMaxJumpSpeed);
-		m_canWalk = false;
+		m_onGround = false;
 		m_state = State::Jump;
 	}
+}
+
+void Player::jumpHandler(float dt)
+{
+	if (m_state != State::Jump) return;
+	
 	/* Update vertical speed. */
-	if(m_state == State::Jump) m_body.setVerticalVelocity(m_body.getVerticalVelocity() - gameData::gravityAcceleration * dt);
+	m_body.setVerticalVelocity(m_body.getVerticalVelocity() - gameData::gravityAcceleration * dt);
 
 	/* End of jump. */
 	if (m_body.getVerticalPosition() < gameData::groundLevel) 
@@ -70,19 +93,86 @@ void Player::jump(float dt)
 		m_body.setVerticalPosition(gameData::groundLevel);
 		m_body.setVerticalVelocity(0.0f);
 		m_state = State::Idle;
-		m_canWalk = true;
+		m_onGround = true;
 	}
 
 }
 
-void Player::update(float dt)
+void Player::setAttack()
 {
-	updateAnimation();
-
-	m_body.updatePosition(dt);
-	m_sprite.updateTexture(dt,m_facingRight);
+	/* First time entering the function. P1 has to be moveable and on ground. */
+	if (m_state != State::Attack && m_canMove && m_onGround)
+	{
+		m_state = State::Attack;
+		m_canMove = false;
+		m_body.setHorizontalVelocity(0.0f);	// stops the character
+	}
 }
 
+void Player::attackHandler(ProjectileManager& projectilesSys)
+{
+	// enter function only if State::Attack is ongoing since 2+ frames
+	if (m_state != State::Attack) return;	
+
+	bool shootingFrameCond = m_sprite.getAnimator().getFrame() == gameData::playerShootFrame;
+	
+	if (shootingFrameCond && !m_shotFired)
+	{
+		shootArrow(projectilesSys);
+		m_shotFired = true;		// shoots only once
+	}
+	else if (m_sprite.getAnimator().isAnimationFinished())
+	{
+		m_state = State::Idle;
+		m_canMove = true;
+		m_shotFired = false;	// can shoot again
+	}
+
+}
+
+void Player::shootArrow(ProjectileManager& projectilesSys)
+{	
+	/* Projectile position adjusted for crossbow height. */
+	glm::vec2 pos = m_body.getPosition() + glm::vec2(0.0f, 0.1f * m_body.getSize().y);
+	if (m_facingRight) pos.x += 0.55f * m_body.getSize().x;
+	else pos.x -= 0.05f * m_body.getSize().x;
+
+	/* Projectile velocity. */
+	glm::vec2 velocity = gameData::arrowSpeed * glm::vec2(1.0f, 0.0f);	// speed * direction
+	if(!m_facingRight) velocity *= -1;		// invert speed if facing left
+
+	/* Creates a projectile with given position, speed, size, verse (+-facingRight). */
+	projectilesSys.emit(gameData::arrowTexName, pos, gameData::arrowSize, velocity, m_facingRight);
+}
+
+void Player::printNewState(const ActionMap& actionMap2D)
+{
+	static int count = -1;
+	if (m_prevState != m_state) 
+	{
+		std::cout << "state: " << stateToString(m_state) << 1+count++ << std::endl;
+		if(count!=0) std::cout << "prevState: " << stateToString(m_prevState) << count-1 << std::endl;
+		std::cout << "canMove: " << (bool)m_canMove << std::endl;
+		std::cout << "onGround: " << (bool)m_onGround << std::endl;
+		std::cout << "isIdle: " << (bool)isIdle(actionMap2D) << std::endl;
+		std::cout << " ----------------------------- " << std::endl;
+
+	}
+}
+
+void Player::update(float dt, ProjectileManager& projectileSys)
+{
+	/* Texture update.*/
+	updateAnimation();
+	m_sprite.updateTexture(dt,m_facingRight);
+
+	/* Position update. */
+	m_body.updatePosition(dt);
+
+	/* States behaviour update. */
+	jumpHandler(dt);
+	attackHandler(projectileSys);
+}
 
 void Player::render(const SpriteRenderer& spriteRenderer, Shader& shader) const
 {
@@ -97,7 +187,8 @@ void Player::resetPlayer()
 	m_state = State::Idle;
 	m_prevState = State::Walk;		
 	m_facingRight = true;
-	m_canWalk = true;
+	m_onGround = true;
+	m_canMove = true;
 	
 
 }
--- src/app/game2D/player.h
+++ src/app/game2D/player.h
@@ -5,22 +5,18 @@
 #include "../../core/action_map.h"
 #include "state.h"
 
+class ProjectileManager;	// fwd decl
+
 class Player
 {
 public:
-	Player(const std::string texName, 
-		glm::vec2 position,
-		glm::vec2 size,
-		float walkSpeed);
+	Player(const std::string& texName, glm::vec2 position, glm::vec2 size, glm::vec2 velocity);
 
+	/* Input manager. Sets states. */
+	void inputHandler(const ActionMap& actionMap2D, float dt, ProjectileManager& projectilesSys);
 
-	/* Input manager. */
-	void inputHandler(const ActionMap& actionMap2D, float dt);
-
-
-
-	/* Update graphics (sprite) and logic (body). */
-	void update(float dt);
+	/* Update graphics (sprite) position, velocity (body) and behaviour from states. */
+	void update(float dt, ProjectileManager& projectilesSys);
 
 	/* Position and size are entangled with its GameObj body, so they would be meaningless as external inputs. */
 	void render(const SpriteRenderer& spriteRenderer, Shader& shader) const;
@@ -44,22 +40,34 @@ private:
 	Sprite m_sprite;
 	/* Current state. */
 	State m_state;
-	/* Previous state. */
+	/* Previous state. Set in updateAnimation(). */
 	State m_prevState;
 
 	/* Sets animation for the sprite. */
 	void setAnimation(int start, int end, int line, float fps);
-	/* Separated from update to keep it clean. */
+	/* Separated from update to keep it clean.
+	 * Utilizes a map to set animation from gameData namespace.
+	 * Sets prevState from actual state. */
 	void updateAnimation();
 
 	/* Game logic variables and methods.*/
 
 	bool m_facingRight = true;
-	//bool m_isJumping = false;
-	bool m_canWalk = true;	// false while jumping / attacking / dying (and after death ofc)
+	bool m_onGround = true;	// false while jumping
+	bool m_canMove = true;	// false while shooting
+	bool m_shotFired = false;	// true after shot, reset false when attack animation ends
 
 	bool isIdle(const ActionMap& actionMap2D);
 	void atIdle();
 	void walk(bool facingRight);
-	void jump(float dt);
+	void setJump();
+	void jumpHandler(float dt);
+	void setAttack();
+	void attackHandler(ProjectileManager& projectilesSys);
+
+	/* Emits projectile object. */
+	void shootArrow(ProjectileManager& projectilesSys);
+
+	/* Prints state when there is a variation.*/
+	void printNewState(const ActionMap& actionMap2D);
 };
