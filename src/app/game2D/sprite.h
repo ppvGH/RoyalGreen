fe40099c59dfbf8a64fca48f90bf6283ad696a3c
ppv <pierpaolo.vesce@studio.unibo.it>
2025-10-31 18:40:29 +0100

    feat(sprite): Add method to retrieve animator

--- src/app/game2D/sprite.h
+++ src/app/game2D/sprite.h
@@ -20,12 +20,12 @@ public:
 	/* Sheet rows and cols are only for sprite sheets. */
 	Sprite(const std::string texName, int sheetRows = 1, int sheetColumns = 1);
 
-
 	void setAnimation(int start, int end, int line, float fps);
 	void updateTexture(float dt, bool facingRight);	// name clearer than just update
 	void render(const SpriteRenderer& spriteRenderer, Shader& shader, glm::vec2 position, glm::vec2 size) const;
 
 	const Texture& getTex() const { return m_tex; }
+	const Animator& getAnimator() const { return m_animator; }
 
 private:
 	Texture& m_tex;
